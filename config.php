948c585a3b418fa9157b79bc1849a22ef8c58781
Victor Oliveira <victor.a.oliveira16@aluno.senai.br>
2026-03-25 08:31:18 -0300

    conexão

--- config.php
+++ config.php
@@ -2,11 +2,18 @@
 $host = 'localhost';
 $db   = 'livraria_livh';
 $user = 'root';
-$pass = ''; // senha padrão do XAMPP é vazia
+$pass = ''; // Senha padrão do XAMPP é vazia
 
 try {
+    // Criando a conexão PDO
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+
+    // Configura o PDO para lançar exceções em caso de erro
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // echo "Conexão realizada com sucesso!"; 
 } catch (PDOException $e) {
+    // Caso a conexão falhe, exibe o erro
     die("Erro ao conectar: " . $e->getMessage());
 }
 ?>
